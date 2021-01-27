1f9cbe3a4cfe58460292402d23013e7a15841c78
Shuvaethy-Neill <shuvaethy.neill@gmail.com>
2021-01-27 18:27:59 -0500

    Update Product.java

--- Milestone 1/src/Product.java
+++ Milestone 1/src/Product.java
@@ -1,26 +1,43 @@
 //Andre Hazim 101141843
-public class Product {
-    private String name;
-    private int id;
-    private double price;
+//This class stores information about the items being sold by the store
 
-    public Product(){
-        name = "";
-        id = 0;
-        price = 0.0;
+public class Product {
+    private String name;    //The name of the product
+    private int id;         //The id of the product
+    private double price;   //The price of the product
+
+    /**
+     * Creates a new product with the supplied attributes.
+     * The product id and price is by default 0.
+     */
+    public Product() {
+        this("", 0, 0.0);
     }
-    public Product(String iName, int iId, double iPrice){
-        name = iName;
-        id = iId;
-        price = iPrice;
+
+    public Product(String name, int id, double price){
+        this.name = name;
+        this.id = id;
+        this.price = price;
     }
-    public String getName(){
+
+    /**
+     * Get the name of the product
+     */
+    public String getName() {
         return name;
     }
-    public int getId(){
+
+    /**
+     * Get the id of the product
+     */
+    public int getId() {
         return id;
     }
-    public double getPrice(){
+
+    /**
+     * Get the price of the product
+     */
+    public double getPrice() {
         return price;
     }
 }
