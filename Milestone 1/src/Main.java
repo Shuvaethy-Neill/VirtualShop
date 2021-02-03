60dc4783afd973ab8ae1b5ab22ac4fbc976bed20
Shuvaethy-Neill <shuvaethy.neill@gmail.com>
2021-02-02 21:54:39 -0500

    Add files via upload
    Fixed StoreManager

--- Milestone 1/src/Main.java
+++ Milestone 1/src/Main.java
@@ -3,30 +3,31 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
 
-        StoreManager testStore = new StoreManager();
+        Inventory managerInventory = new Inventory();
+        StoreManager testStore = new StoreManager(managerInventory);
         Product p1 = new Product("ball", 1, 3.40);
         Product p2 = new Product("triangle", 2, 3.70);
         Product p3 = new Product("Computer", 3, 100);
 
-        testStore.managerInventory.addStock(p1, 3);
-        testStore.managerInventory.addStock(p2, 3);
+        managerInventory.addStock(p1, 3);
+        managerInventory.addStock(p2, 3);
 
-        testStore.managerInventory.addStock(p3,1);
-        testStore.managerInventory.addStock(p2, 2);
-        testStore.managerInventory.addStock(p3,4);
-        int ballStock = testStore.managerInventory.getStock(1);
-        int triangleStock = testStore.managerInventory.getStock(2);
-        int computerStock = testStore.managerInventory.getStock(3);
+        managerInventory.addStock(p3,1);
+        managerInventory.addStock(p2, 2);
+        managerInventory.addStock(p3,4);
+        int ballStock = managerInventory.getStock(1);
+        int triangleStock = managerInventory.getStock(2);
+        int computerStock = managerInventory.getStock(3);
 
-        String productName = testStore.managerInventory.getProductName(2);
+        String productName = managerInventory.getProductName(2);
 
         System.out.println("ball stock: "+ ballStock +", Triangle Stock: "+ triangleStock +
                 ", Computer Stock: " + computerStock);
         System.out.println(productName);
 
-        testStore.managerInventory.productInfo(1);
-        testStore.managerInventory.productInfo(2);
-        testStore.managerInventory.productInfo(3);
+        managerInventory.productInfo(1);
+        managerInventory.productInfo(2);
+        managerInventory.productInfo(3);
 
         // Create an order as a 2D array
         int[][] order1 = {{p1.getId(), 3}, {p2.getId(), 1}, {p3.getId(), 2}};
@@ -35,9 +36,9 @@ public class Main {
 
         System.out.println("Your total comes to: " + testStore.transaction(order1));
 
-        testStore.managerInventory.productInfo(1);
-        testStore.managerInventory.productInfo(2);
-        testStore.managerInventory.productInfo(3);
+        managerInventory.productInfo(1);
+        managerInventory.productInfo(2);
+        managerInventory.productInfo(3);
 
         // Create an order to test the out of stock case
         int[][] order2 = {{p1.getId(), 1}, {p2.getId(), 1}, {p3.getId(), 2}};
--- Milestone 1/src/StoreManager.java
+++ Milestone 1/src/StoreManager.java
@@ -1,11 +1,15 @@
-//Milestone 1
-//Shuvaethy Neill 101143478
+// Milestone 1
+// Shuvaethy Neill 101143478
 
 public class StoreManager {
-    public Inventory managerInventory;
+    private Inventory managerInventory;
 
     public StoreManager(){
         managerInventory = new Inventory();
+    }
+
+    public StoreManager(Inventory inv){
+        managerInventory = inv;
     }
 
     public int getInventoryStock(int productID){
@@ -19,24 +23,24 @@ public class StoreManager {
 
     public double transaction(int [][] order){
         double total = 0;
-        int rows = order.length;    //rows represents each product and quantity [ProductID1, quantity
-        for(int i = 0; i < rows; i++ ){     //in each row
+        int rows = order.length;    // rows represents each product and quantity [ProductID1, quantity]
+        for(int i = 0; i < rows; i++ ){     // in each row
 
-            int j = order[i][0];    //j is the productID and k is the quantity
-            int k = order[i][1];
+            int productID = order[i][0];
+            int quantity = order[i][1];
 
-            int stock = getInventoryStock(j);
+            int stock = getInventoryStock(productID);
 
-            if(k > getInventoryStock(j)) {  //if quantity is greater than stock indicate error by returning -1
+            if(quantity > getInventoryStock(productID)) {  // if quantity is greater than stock indicate error by returning -1
                 System.out.println("Sorry one of the products you were looking to purchase is out of stock");
                 return -1;
             }
 
-            else{   //if (k <= getInventoryStock(j)){
-                 total += (k * managerInventory.getPrice(j));   //add everything to get total
-                 removeInventoryStock(j, k);    //subtract the quantity purchased from the product's stock after successful transaction
+            else{   // if (k <= getInventoryStock(j)){
+                 total += (quantity * managerInventory.getPrice(productID));   // add everything to get total
+                 removeInventoryStock(productID, quantity);    // subtract the quantity purchased from the product's stock after successful transaction
             }
         }
-        return Math.round(total); //returns it rounded to two decimals
+        return Math.round(total); // returns it rounded to two decimals
     }
 }
