760f3ec60e3fbef72b1a0f162f06b3a126eeee91
andre-Hazim <andre.hazim1@gmail.com>
2021-03-07 10:24:09 -0500

    Update ShoppingCart.java

--- Milestone 1/src/ShoppingCart.java
+++ Milestone 1/src/ShoppingCart.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.Locale;
 
-public class ShoppingCart extends Inventory{
+public class ShoppingCart {
     private ArrayList<Product>cart;
     private ArrayList<Integer>itemsInCart;
     private int cartID;
@@ -13,38 +13,6 @@ public class ShoppingCart extends Inventory{
         this.cart = new ArrayList<Product>();
         this.itemsInCart = new ArrayList<Integer>();
         this.cartID = cartID;
-    }
-
-
-    public String getProductName(int productID){
-        int i = 0;
-        while(cart.get(i).getId() != productID && i<cart.size()-1){
-            i++;
-        }
-        String productName = cart.get(i).getName();
-
-        // If the product does not exist return null
-        if(cart.get(i).getId() != productID){
-            productName = null;
-        }
-
-        return productName;
-    }
-    public int getStock(int productID) {
-        int stock = 0;
-        if (cart.isEmpty() == false) {
-            int i = 0;
-            while (cart.get(i).getId() != productID && i < cart.size()-1) {
-                i++;
-            }
-            stock = itemsInCart.get(i);
-
-            // If product does not exist return -1
-            if (cart.get(i).getId() != productID) {
-                stock = -1;
-            }
-        }
-        return stock;
     }
 
     public ArrayList<Product> getCart(){
