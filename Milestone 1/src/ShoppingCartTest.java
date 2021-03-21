510d373c3adea60d88dcbdad83a69a8eb15dd481
Shuvaethy-Neill <shuvaethy.neill@gmail.com>
2021-03-21 01:58:54 -0400

    added docstrings and changed BeforeAll to BeforeEach

--- Milestone 1/src/ShoppingCartTest.java
+++ Milestone 1/src/ShoppingCartTest.java
@@ -14,6 +14,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * This ShoppingCartTest class tests the functionality of the ShoppingCart class
+ *
  * @author Shuvaethy Neill
  *
  * @version 1.0
@@ -26,88 +27,88 @@ public class ShoppingCartTest {
     private static ShoppingCart shoppingCart;
     private static StoreManager storeManager;
 
-
     /**
-     *
+     * Initialize the variables used throughout the test suite
      */
-    @BeforeAll
-    public static void init() {
-        shoppingCart = new ShoppingCart();
+    @BeforeEach
+    public void init() {
+        // Executed before each test
         storeManager = new StoreManager();
+        shoppingCart = new ShoppingCart(storeManager.generateCartID());
     }
 
     /**
-     *
+     * Tests the getCart method to ensure the correct cart products are returned
      */
     @Test
     public void testGetCart(){
-        System.out.println("--------------------------------------------------------------------------------------------------------------");
         System.out.println("Testing the getCart method in the ShoppingCart Class");
         System.out.println();
 
-        int cartID = storeManager.generateCartID();
-        ShoppingCart shoppingCart = new ShoppingCart(cartID);
         storeManager.addShoppingCart(shoppingCart);
-        ArrayList<Product> cart = new ArrayList<>();
-        assertEquals(cart, shoppingCart.getCart(), "There is a bug in the getCart method in the ShoppingCart class");
 
-        storeManager.addToCart(1, 1, cartID);
+        // Create test array list representing a cart
+        ArrayList<Product> cart = new ArrayList<>();
+
+        // Testing with an empty cart (no products)
+        assertEquals(cart, shoppingCart.getCart(), "The cart should be empty. There is a bug in the getCart method in the ShoppingCart class.");
+
+        storeManager.addToCart(1, 1, 0);
         cart.add(storeManager.getStoreInventory().getProduct(1));
         assertEquals(cart, shoppingCart.getCart(), "There is a bug in the getCart method in the ShoppingCart class");
 
-        storeManager.addToCart(3, 7, cartID);
+        storeManager.addToCart(3, 7, 0);
         cart.add(storeManager.getStoreInventory().getProduct(3));
         assertEquals(cart, shoppingCart.getCart(), "There is a bug in the getCart method in the ShoppingCart class");
 
-        storeManager.removeFromCart(1, 2, cartID);
+        storeManager.removeFromCart(1, 2, 0);
         cart.remove(storeManager.getStoreInventory().getProduct(1));
         assertEquals(cart, shoppingCart.getCart(), "There is a bug in the getCart method in the ShoppingCart class");
 
-        storeManager.addToCart(5, 3, cartID);
+        storeManager.addToCart(5, 3, 0);
         cart.add(storeManager.getStoreInventory().getProduct(5));
         assertEquals(cart, shoppingCart.getCart(), "There is a bug in the getCart method in the ShoppingCart class");
-
     }
 
     /**
-     *
+     * Tests the getItemsInCart to ensure the correct item quantities in the cart are returned
      */
     @Test
     public void testGetItemsInCart(){
-        System.out.println("--------------------------------------------------------------------------------------------------------------");
         System.out.println("Testing the getItemsInCart method in the ShoppingCart Class");
         System.out.println();
 
-        int cartID = storeManager.generateCartID();
-        ShoppingCart shoppingCart = new ShoppingCart(cartID);
         storeManager.addShoppingCart(shoppingCart);
-        ArrayList<Integer> cartItems = new ArrayList<>();
-        assertEquals(cartItems, shoppingCart.getItemsInCart(), "There is a bug in the getItemsInCart method in the ShoppingCart class");
 
-        storeManager.addToCart(1, 1,cartID);
+        // Create test array list representing the items (quantities) in a cart
+        ArrayList<Integer> cartItems = new ArrayList<>();
+
+        // Testing with an empty cart (no quantity of items)
+        assertEquals(cartItems, shoppingCart.getItemsInCart(), "The cart should be empty. There is a bug in the getItemsInCart method in the ShoppingCart class");
+
+        storeManager.addToCart(1, 1,0);
         cartItems.add(1);
         assertEquals(cartItems, shoppingCart.getItemsInCart(), "There is a bug in the getItemsInCart method in the ShoppingCart class");
 
-        storeManager.addToCart(1, 5,cartID);
+        storeManager.addToCart(1, 5,0);
         cartItems.set(0, 6);
         assertEquals(cartItems, shoppingCart.getItemsInCart(), "There is a bug in the getItemsInCart method in the ShoppingCart class");
 
-        storeManager.addToCart(3, 20,cartID);
+        storeManager.addToCart(3, 20,0);
         cartItems.add(20);
         assertEquals(cartItems, shoppingCart.getItemsInCart(), "There is a bug in the getItemsInCart method in the ShoppingCart class");
 
-        storeManager.removeFromCart(1, 9, cartID);
+        storeManager.removeFromCart(1, 9, 0);
         cartItems.remove(0);
         assertEquals(cartItems, shoppingCart.getItemsInCart(), "There is a bug in the getItemsInCart method in the ShoppingCart class");
 
-        storeManager.addToCart(4, 3, cartID);
+        storeManager.addToCart(4, 3, 0);
         cartItems.add(3);
         assertEquals(cartItems, shoppingCart.getItemsInCart(), "There is a bug in the getItemsInCart method in the ShoppingCart class");
 
-        storeManager.removeFromCart(3, 2, cartID);
+        storeManager.removeFromCart(3, 2, 0);
         cartItems.set(0, 18);
         assertEquals(cartItems, shoppingCart.getItemsInCart(), "There is a bug in the getItemsInCart method in the ShoppingCart class");
 
     }
-
 }
