aa72f81260e95f057a9d0c5781914d31fcdacb42
Shuvaethy-Neill <shuvaethy.neill@gmail.com>
2021-03-22 01:08:43 -0400

    final milestone 3

--- Milestone 1/src/storetest/StoreManagerTest.java
+++ Milestone 1/src/storetest/StoreManagerTest.java
@@ -1,22 +1,20 @@
 // Written by: Shuvaethy Neill 101143478
 // Partner: Andre Hazim 101141843
 
-
+package storetest;
 
 import org.junit.jupiter.api.*;
 import store.Inventory;
 import store.Product;
 import store.ShoppingCart;
 import store.StoreManager;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
- * This StoreManagerTest class tests the functionality of the store.store.StoreManager class
+ * This StoreManagerTest class tests the functionality of the StoreManager class
  *
  * @author Shuvaethy Neill
  *
@@ -40,19 +38,6 @@ public class StoreManagerTest {
         inv = new Inventory();
         storeManager = new StoreManager(inv);
         shoppingCart = new ShoppingCart(storeManager.generateCartID());
-
-    }
-
-    /**
-     * Tests the getStoreInventory method to make sure the correct inventory is returned
-     */
-    @Test
-    public void testGetStoreInventory(){
-        System.out.println("Testing the getStoreInventory method in the store.StoreManager Class");
-        System.out.println();
-
-        //How do you test for the case where an inv does not already exist?
-        assertEquals(inv, storeManager.getStoreInventory(), "There is a bug in the getStoreInventory method in the store.StoreManager class");
     }
 
     /**
@@ -60,30 +45,30 @@ public class StoreManagerTest {
      */
     @Test
     public void testCheckInventoryStock(){
-        System.out.println("Testing the checkInventoryStock method in the store.StoreManager Class");
+        System.out.println("Testing the checkInventoryStock method in the StoreManager Class");
         System.out.println();
 
-        assertEquals(50, storeManager.checkInventoryStock(inv.getProduct(1)), "There is a bug in the checkInventoryStock method in the store.StoreManager Class");
+        assertEquals(50, storeManager.checkInventoryStock(inv.getProduct(1)), "There is a bug in the checkInventoryStock method in the StoreManager Class");
 
         inv.removeStock(3, 20);
-        assertEquals(30, storeManager.checkInventoryStock(inv.getProduct(3)), "There is a bug in the checkInventoryStock method in the store.StoreManager Class");
+        assertEquals(30, storeManager.checkInventoryStock(inv.getProduct(3)), "There is a bug in the checkInventoryStock method in the StoreManager Class");
 
         inv.addStock(inv.getProduct(3), 1);
-        assertEquals(31, storeManager.checkInventoryStock(inv.getProduct(3)), "There is a bug in the checkInventoryStock method in the store.StoreManager Class");
+        assertEquals(31, storeManager.checkInventoryStock(inv.getProduct(3)), "There is a bug in the checkInventoryStock method in the StoreManager Class");
 
         // More than what exists is removed (stock should be 0)
         inv.removeStock(3, 35);
-        assertEquals(0, storeManager.checkInventoryStock(inv.getProduct(3)), "There is a bug in the checkInventoryStock method in the store.StoreManager Class");
+        assertEquals(0, storeManager.checkInventoryStock(inv.getProduct(3)), "There is a bug in the checkInventoryStock method in the StoreManager Class");
 
         // More than what exists is removed (stock should be 0)
         inv.removeStock(1, 55);
-        assertEquals(0, storeManager.checkInventoryStock(inv.getProduct(1)), "There is a bug in the checkInventoryStock method in the store.StoreManager Class");
+        assertEquals(0, storeManager.checkInventoryStock(inv.getProduct(1)), "There is a bug in the checkInventoryStock method in the StoreManager Class");
 
         // Invalid productID (does not exist - should return -1)
-        assertEquals(-1, storeManager.checkInventoryStock(inv.getProduct(8)), "There is a bug in the checkInventoryStock method in the store.StoreManager Class");
+        assertEquals(-1, storeManager.checkInventoryStock(inv.getProduct(8)), "There is a bug in the checkInventoryStock method in the StoreManager Class");
 
         // Invalid productID (does not exist - should return -1)
-        assertEquals(-1, storeManager.checkInventoryStock(inv.getProduct(-2)), "There is a bug in the checkInventoryStock method in the store.StoreManager Class");
+        assertEquals(-1, storeManager.checkInventoryStock(inv.getProduct(-2)), "There is a bug in the checkInventoryStock method in the StoreManager Class");
 
     }
 
@@ -101,97 +86,29 @@ public class StoreManagerTest {
     }
 
     /**
-     *
+     * Tests the addShoppingCart method to make sure a cart is correctly added with it's cartID
      */
     @Test
     public void testAddShoppingCart(){
+        System.out.println("Testing the addShoppingCart method in the StoreManager Class");
+        System.out.println();
 
-
-        int cartID = storeManager.generateCartID();
-        store.ShoppingCart shoppingCart = new store.ShoppingCart(cartID);
         storeManager.addShoppingCart(shoppingCart);
 
         HashMap<Integer,ShoppingCart> testCarts = new HashMap<Integer,ShoppingCart>();
-        testCarts.put(cartID,shoppingCart);
+        testCarts.put(0,shoppingCart);
 
         assertEquals(testCarts, storeManager.getCarts(), "There is something wrong with the addShoppingCart method");
-
-
-
     }
 
     /**
-     * Tests the getSMCart method to make sure the correct cart products are returned
-     */
-    @Test
-    public void testGetSMCart(){
-        System.out.println("Testing the getSMCart method in the store.StoreManager Class");
-        System.out.println();
-
-        storeManager.addShoppingCart(shoppingCart);
-        storeManager.getSMCart(0);
-
-        // Create test cart to compare
-        ArrayList<Product> testSMCart = new ArrayList<Product>();
-        
-        //Testing with an empty cart
-        assertEquals(testSMCart, storeManager.getSMCart(0), "Cart should be empty. There is a bug in the getSMCart method in the store.StoreManager class");
-
-        storeManager.addToCart(1, 1, 0);
-        testSMCart.add(storeManager.getStoreInventory().getProduct(1));
-        assertEquals(testSMCart, storeManager.getSMCart(0), "There is a bug in the getSMCart method in the store.StoreManager class");
-
-        storeManager.addToCart(2, 3, 0);
-        testSMCart.add(storeManager.getStoreInventory().getProduct(2));
-        assertEquals(testSMCart, storeManager.getSMCart(0), "There is a bug in the getSMCart method in the store.StoreManager class");
-
-        storeManager.removeFromCart(1, 5, 0);
-        testSMCart.remove(storeManager.getStoreInventory().getProduct(1));
-        assertEquals(testSMCart, storeManager.getSMCart(0), "There is a bug in the getSMCart method in the store.StoreManager class");
-    }
-
-    /**
-     * Tests the getSMItemsInCart method to make sure the correct item quantities in the cart are returned
-     */
-    @Test
-    public void testGetSMItemsInCart(){
-        System.out.println("Testing the getSMItemsInCart method in the store.StoreManager Class");
-        System.out.println();
-
-        storeManager.addShoppingCart(shoppingCart);
-        storeManager.getSMCart(0);
-
-        // Create test array list of item quantities to compare
-        ArrayList<Integer> testSMItemsInCart = new ArrayList<Integer>();
-        
-        // Testing with empty cart
-        assertEquals(testSMItemsInCart, storeManager.getSMItemsInCart(0), "There is a bug in the getSMCart method in the store.StoreManager class");
-
-        storeManager.addToCart(1, 1, 0);
-        testSMItemsInCart.add(1);
-        assertEquals(testSMItemsInCart, storeManager.getSMItemsInCart(0), "There is a bug in the getSMCart method in the store.StoreManager class");
-
-        storeManager.addToCart(2, 3, 0);
-        testSMItemsInCart.add(3);
-        assertEquals(testSMItemsInCart, storeManager.getSMItemsInCart(0), "There is a bug in the getSMCart method in the store.StoreManager class");
-
-        // Completely removing and item from cart (quantity should be completely removed)
-        storeManager.removeFromCart(1, 5, 0);
-        testSMItemsInCart.remove(0);
-        assertEquals(testSMItemsInCart, storeManager.getSMItemsInCart(0), "There is a bug in the getSMCart method in the store.StoreManager class");
-
-        storeManager.removeFromCart(2, 1, 0);
-        testSMItemsInCart.set(0, 2);
-        assertEquals(testSMItemsInCart, storeManager.getSMItemsInCart(0), "There is a bug in the getSMCart method in the store.StoreManager class");
-    }
-
-    /**
-     * Tests the addToCart method to make sure iems are correctly added to a cart
+     * Tests the addToCart method to make sure items are correctly added to a cart
      */
     @Test
     public void testAddToCart(){
-        System.out.println("Testing the addToCart method in the store.StoreManager Class");
+        System.out.println("Testing the addToCart method in the StoreManager Class");
         System.out.println();
+
 
         storeManager.addShoppingCart(shoppingCart);
 
@@ -200,12 +117,12 @@ public class StoreManagerTest {
         ArrayList<Integer> tempItemsInCart = new ArrayList<Integer>();
 
         storeManager.addToCart(1,2,0);
-        
-        tempCart.add(storeManager.getStoreInventory().getProduct(1));
+
+        tempCart.add(inv.getProduct(1));
         tempItemsInCart.add(2);
-        assertEquals(tempCart, shoppingCart.getCart(), "There is a bug in the addToCart method in the store.StoreManager class");
-        assertEquals(tempItemsInCart, shoppingCart.getItemsInCart(), "There is a bug in the addToCart method in the store.StoreManager class");
-        assertEquals(48, storeManager.getStoreInventory().getStock(1), "There is a bug in the addToCart method in the store.StoreManager class");
+        assertEquals(tempCart, shoppingCart.getCart(), "There is a bug in the addToCart method in the StoreManager class");
+        assertEquals(tempItemsInCart, shoppingCart.getItemsInCart(), "There is a bug in the addToCart method in the StoreManager class");
+        assertEquals(48, storeManager.getStoreInventory().getStock(1), "There is a bug in the addToCart method in the StoreManager class");
 
         storeManager.addToCart(4,15,0);
 
@@ -213,14 +130,14 @@ public class StoreManagerTest {
         tempItemsInCart.add(15);
         assertEquals(tempCart, shoppingCart.getCart(), "There is a bug in the addToCart method in the store.StoreManager class");
         assertEquals(tempItemsInCart, shoppingCart.getItemsInCart(), "There is a bug in the addToCart method in the store.StoreManager class");
-        assertEquals(35, storeManager.getStoreInventory().getStock(4), "There is a bug in the addToCart method in the store.StoreManager class");
+        assertEquals(35, storeManager.getStoreInventory().getStock(4), "There is a bug in the addToCart method in the StoreManager class");
 
         storeManager.addToCart(1,25,0);
 
         tempItemsInCart.set(0,27);
         assertEquals(tempCart, shoppingCart.getCart(), "There is a bug in the addToCart method in the store.StoreManager class");
         assertEquals(tempItemsInCart, shoppingCart.getItemsInCart(), "There is a bug in the addToCart method in the store.StoreManager class");
-        assertEquals(23, storeManager.getStoreInventory().getStock(1), "There is a bug in the addToCart method in the store.StoreManager class");
+        assertEquals(23, storeManager.getStoreInventory().getStock(1), "There is a bug in the addToCart method in the StoreManager class");
 
     }
 
@@ -233,7 +150,7 @@ public class StoreManagerTest {
         System.out.println();
 
         storeManager.addShoppingCart(shoppingCart);
-        
+
         // Create a test cart (products and their quantities)
         ArrayList<Product> tempCart = new ArrayList<Product>();
         ArrayList<Integer> tempItemsInCart = new ArrayList<Integer>();
