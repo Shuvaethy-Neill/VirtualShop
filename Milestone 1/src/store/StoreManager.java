cc2a9ee90d89774e1cefa0b19111c8f317807875
Shuvaethy-Neill <shuvaethy.neill@gmail.com>
2021-03-22 01:05:24 -0400

    final milestone 3

--- Milestone 1/src/store/StoreManager.java
+++ Milestone 1/src/store/StoreManager.java
@@ -8,7 +8,7 @@ import java.util.HashMap;
 
 /**
  * This store.StoreManager class is the "brain" of the system and for this
- * milestone it manages the store.Inventory and ShoppingCarts.
+ * milestone it manages the Inventory and ShoppingCarts.
  * @author Shuvaethy Neill
  *
  * @version 2.0
@@ -23,7 +23,7 @@ public class StoreManager {
     private HashMap <Integer, ShoppingCart> carts;      // The cartIDs and ShoppingCarts created
 
     /**
-     * Default constructor for store.StoreManager
+     * Default constructor for StoreManager
      *
      */
     public StoreManager(){
@@ -33,9 +33,9 @@ public class StoreManager {
     }
 
     /**
-     * Constructor for store.StoreManager
+     * Constructor for StoreManager
      *
-     * @param inv store.Inventory, existing store.Inventory
+     * @param inv Inventory, existing store.Inventory
      */
     public StoreManager(Inventory inv){
         managerInventory = inv;
@@ -54,7 +54,7 @@ public class StoreManager {
 
     /**
      * This method checks the available stock of a product given its object
-     * @param product store.Product, the product to be checked
+     * @param product Product, the product to be checked
      * @return int, value of stock available for that specific product
      */
     public int checkInventoryStock(Product product){
@@ -66,7 +66,7 @@ public class StoreManager {
 
     /**
      * This method keeps track of how many cartIDs there are and generates a new cartID
-     * everytime a new store.ShoppingCart is made
+     * everytime a new ShoppingCart is made
      *
      * @return int, new cartID
      */
@@ -76,10 +76,10 @@ public class StoreManager {
     }
 
     /**
-     * This method adds a store.ShoppingCart along with its cartID to
+     * This method adds a ShoppingCart along with its cartID to
      * carts (the HashMap that keeps track of the cartIDs and their shopping carts)
      *
-     * @param shoppingCart store.ShoppingCart, the new shopping cart being added
+     * @param shoppingCart ShoppingCart, the new shopping cart being added
      */
     public void addShoppingCart(ShoppingCart shoppingCart){
         this.carts.put(numCarts, shoppingCart);
@@ -89,7 +89,7 @@ public class StoreManager {
      * This method gets the cart of the shopping cart given its cartID
      *
      * @param cartId int, the cartID associated with the specific shopping cart
-     * @return ArrayList<store.Product>, the arraylist of the cart of the shopping cart
+     * @return ArrayList<Product>, the arraylist of the cart of the shopping cart
      */
     public ArrayList<Product> getSMCart(int cartId){
         ShoppingCart shoppingCart = carts.get(cartId);
@@ -117,7 +117,7 @@ public class StoreManager {
     public void addToCart(int productID, int amountOfProduct, int cartId){
         ShoppingCart shoppingCart = carts.get(cartId);
 
-        // If cart is empty, simply add the item and its quantity
+
         shoppingCart.addToCart(productID,amountOfProduct,this.getStoreInventory());
         // Update the carts HashMap with the new additions
         carts.put(cartId, shoppingCart);
